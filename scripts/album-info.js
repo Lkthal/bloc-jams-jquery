d04a75a33da84e70e4b8cc5533efd97b3176d143
Thal Syhabout <tsyhabout@gmail.com>
2018-01-01 23:22:37 -0800

    album info complete

---
+++ scripts/album-info.js
@@ -0,0 +1,9 @@
+$( document ).ready(){
+
+  $('#album-title').text(album.title);
+  $('.artist').text(album.artist);
+  $('#release-info').text(album.releaseInfo);
+  $('img#album-cover-art').attr('src', album.albumArtUrl);
+
+
+}
